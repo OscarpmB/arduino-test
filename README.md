48ff9f1cbfe8d55f66521a0f0fbadc51ee5a74eb
Oscar Backström <oscar-backstrom@hotmail.com>
2022-04-16 13:06:56 +0200

    handle on GPIO pin

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # arduino-test
+This is a very simple program that currently only turns on an of some ports which makes an led go on and off.
--- src/led.c
+++ src/led.c
@@ -1,15 +1,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
+//#include <avr/iom328p.h> // finds all imports so vscode doesnot complain
+
+
 
 int main(void)
 {
-    DDRB = DDRB | (1 << DDB5);
+    // Set Data Direction register D pin 6 to write
+    // DDRD = DDRD | 0x40;
+    DDRD |= (1 << 6);
+    DDRB |= (1 << 5);
 
     while(1){
-        PORTB = PORTB | (1 << PORTB5);
-        _delay_ms(1000);
-        PORTB = PORTB & ~(1 << PORTB5);
-        _delay_ms(1000);
+        // set Port D pin 6 to high  0000 0001 ->  0100 0000
+        PORTD |= (1 << PORTD6);
+        PORTB |= (1 << PORTB5);
+        _delay_ms(4000);
+        PORTD &= ~(1 << PORTD6);
+        PORTB &= ~(1 << PORTB5);
+        _delay_ms(4000);
     }
     return 0;
 }
