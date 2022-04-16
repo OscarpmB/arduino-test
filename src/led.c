801c7b6578e48fbdc46e1dac1f633ac703e51ea1
Oscar Backström <oscar-backstrom@hotmail.com>
2022-04-16 13:42:05 +0200

    broke it all

--- src/led.c
+++ src/led.c
@@ -1,17 +1,14 @@
 #include <avr/io.h>
 #include <util/delay.h>
-//#include <avr/iom328p.h> // finds all imports so vscode doesnot complain
+// #include <avr/iom328p.h> // finds all imports so vscode doesnot complain
 
-
-
-int main(void)
-{
+void stupid_blink(double s){
     // Set Data Direction register D pin 6 to write
     // DDRD = DDRD | 0x40;
     DDRD |= (1 << 6);
     DDRB |= (1 << 5);
 
-    while(1){
+    while(0){
         // set Port D pin 6 to high  0000 0001 ->  0100 0000
         PORTD |= (1 << PORTD6);
         PORTB |= (1 << PORTB5);
@@ -20,5 +17,26 @@ int main(void)
         PORTB &= ~(1 << PORTB5);
         _delay_ms(4000);
     }
+}
+
+
+int main(void)
+{
+    DDRD |= (1 << 6); // PIN 6 as output 
+    DDRD &= ~(1 << 5); // PIN 5 AS INPUT
+    DDRD &= ~(1 << 4); // PIN 4 AS INPUT
+
+    DDRB |= (1 << 5);
+
+    while(1){
+        int press = (PIND & (1 << 4));
+        if( press != 1 ){
+            PORTB |= (1 << PORTB5);
+            _delay_ms(10000);
+        }else{
+            PORTB &= ~(1 << PORTB5);
+        }
+    }
+
     return 0;
 }
