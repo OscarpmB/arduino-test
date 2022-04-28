8d123c77a53a53d771e76ba435c7596e24f47aeb
Oscar Backström <oscar-backstrom@hotmail.com>
2022-04-28 22:42:34 +0200

    .

--- src/temperature.c
+++ src/temperature.c
@@ -1,41 +1,32 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/iom328p.h> 
+// #include <avr/iom328p.h> // finds all imports so vscode doesnot complain
 
-    /*
-    PD7 - red light output port
-    PD5 - Green light output port
-    PC0 - Analog in for temrature sensor
-    */
-void init_ports(){
-    // Set pin 7 and 5 output pins
-    DDRD |= 0xA0; 
-    // Set pin 0 on PORTC to 0
-    DDRC &= ~0x01;    
-}
+// void init_leds(){
+//     DDRB = DDRB | (1 << DDB5);
+//     DDRD = DDRD | (1 << DDB7); 
+// }
 
-void test_lights(){
-    int i = 0;
+/*
+    TODO
+    fix why code wont run with functions decleard above main
+*/
+
+int main(int argc, char const *argv[]){
+    // init_ports();
+    // test_lights();
+    DDRB = DDRB | (1 << DDB5);
+    DDRD = DDRD | (1 << DDB7); 
+    // init_leds();
     while(1){
-        if(i == 0){
-            // Turn red light on, green light off
-            PORTD |= (1 << 7);
-            PORTD &= ~(1 << 5);
-            i = 1;
-        }else{
-            // Turn red light off, green light on
-            PORTD &= ~(1 << 7);
-            PORTD |= (1 << 5);
-            i = 0;
-        }
+        DDRD = DDRD | (1 << DDB5); 
+        PORTD = PORTD | (1 << PORT5);
+        _delay_ms(1000);
+        PORTD = PORTD & ~(1 << PORT5);
         _delay_ms(1000);
     }
-}
-
-int main(int argc, char const *argv[])
-{
-    init_ports();
-    test_lights();
 
     return 0;
 }
+
+
